f37480411601501fd8800354013ee0d6368ef18f
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-08-28 08:30:15 +0200

    added watermark config loader to utils

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -39,7 +39,7 @@ from lm_eval.models.utils import (
 )
 
 from lm_eval.watermark.auto_watermark import AutoWatermark
-from lm_eval.watermark.utils import ModelConfig
+from lm_eval.watermark.utils import ModelConfig, load_config_file
 
 eval_logger = utils.eval_logger
 
@@ -271,25 +271,6 @@ class HFLM(TemplateLM):
 
         # watermarking scheme stuff
         if watermarking_scheme:
-        
-            def load_config_file(path: str) -> dict:
-                """Load a JSON configuration file from the specified path and return it as a dictionary."""
-                try:
-                    with open(path, 'r') as f:
-                        config_dict = json.load(f)
-                    return config_dict
-
-                except FileNotFoundError:
-                    print(f"Error: The file '{path}' does not exist.")
-                    return None
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON in '{path}': {e}")
-                    # Handle other potential JSON decoding errors here
-                    return None
-                except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
-                    # Handle other unexpected errors here
-                    return None
 
             # watermarking scheme stuff
             eval_logger.info(f"Watermarking scheme: {watermarking_scheme}")
